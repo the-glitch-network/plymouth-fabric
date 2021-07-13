43bc1ebbf021871de5221d2a7736fe8339782d7f
KJP12 <kjp12.k.joy.programer12@gmail.com>
2021-07-13 01:19:33 -0700

    Pilot; dev branch requires refactoring

--- src/main/java/net/kjp12/helium/mixins/AccessorPlayerEntity.java
+++ src/main/java/net/kjp12/helium/mixins/AccessorPlayerEntity.java
@@ -1,3 +1,9 @@
+/* Copyright (c) 2021 KJP12
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 package net.kjp12.helium.mixins;// Created 2021-02-06T15:30:30
 
 import net.minecraft.entity.player.PlayerEntity;
